378ca03cd5a30553ae9080f908c69176793ac3ea
Zahid Hasan <144550241+emptybrain369@users.noreply.github.com>
2023-12-06 20:18:33 +0600

    C/C++

---
+++ uri_1008.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int A, B;
+double C, SALARY;
+
+scanf("%d", &A);
+scanf("%d", &B);
+scanf("%lf", &C);
+
+SALARY = B * C;
+
+printf("NUMBER = %d\n", A);
+printf("SALARY =  U$ %.2lf\n", SALARY);
+
+    return 0;
+}
